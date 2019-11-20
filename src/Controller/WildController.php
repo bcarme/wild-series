38e6fd6fc511b15d7e78c6eefc822e4354b41f11
Barbara Carmé <barbara_carme@yahoo.fr>
2019-11-20 14:06:02 +0100

    slug

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -4,15 +4,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class WildController extends AbstractController
 {
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/wild/show/{slug}",
+     *      requirements={"slug"="[a-z0-9\-]+"},
+     *      defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
+     *      methods={"GET"},
+     *      name="wild_show")
+     */
+
+    public function show($slug) :Response
+    {
+
+        if (empty($slug)){
+            $slug = "Aucune série sélectionnée, veuillez choisir une série";
+        } else {
+            $title = explode("-", $slug);
+            $newTitle = implode(" ", $title);
+            $slug = ucwords($newTitle);
+        }
+
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
+    }
+
 }
