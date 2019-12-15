60a7fbda6e18c116889e32ed4df48633ee5dc457
Barbara Carmé <barbara_carme@yahoo.fr>
2019-12-15 17:34:41 +0100

    fix bundle

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -13,8 +13,6 @@ use Symfony\Component\Mime\Email;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 /**
  * @Route("/program")
  */
