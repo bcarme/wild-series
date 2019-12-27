9fd030e5a03837434244cd1188c65f03e5d5c0aa
Barbara Carmé <barbara_carme@yahoo.fr>
2019-12-27 12:38:39 +0100

    quete_22 builderquery and 1000 fixtures

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -35,7 +35,7 @@ class WildController extends AbstractController
 
         $programs=$this->getDoctrine()
             ->getRepository(Program::class)
-            ->findAll();
+            ->findAllWithCategoriesAndActors();
 
         if (!$programs){
             throw $this->createNotFoundException('No program found in the table');
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Service\Slugify;
+use Faker;
+use App\Entity\Actor;
+use App\Entity\Program;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +14,35 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('us_US');
+
+        for ($i = 1; $i <= 1000; $i++) {
+            $category = new Category();
+            $category->setName($faker->word);
+            $manager->persist($category);
+            $this->addReference('category_'.$i, $category);
+
+            $program = new Program();
+            $slugify = new Slugify();
+            $program->setTitle($faker->sentence(4, true));
+            $program->setSummary($faker->text(100));
+            $program->setCategory($this->getReference('category_'.$i));
+            $slug = $slugify->generate($program->getTitle());
+            $program->setSlug($slug);
+            $this->addReference('program_'.$i, $program);
+            $manager->persist($program);
+
+            for($j = 1; $j <= 5; $j ++) {
+                $actor = new Actor();
+                $actor->setName($faker->firstName);
+                $actor->addProgram($this->getReference('program_'.$i));
+                $actor->setSlug($slugify->generate($actor->getName()));
+                $manager->persist($actor);
+            }
+
+        }
 
         $manager->flush();
+
     }
 }
